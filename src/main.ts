76e9340b32151e59180b6fb25c7ada1f28bb74f1
Brenda Lee <brenda.matozo@gmail.com>
2023-10-16 19:06:53 -0300

    chore: add paginate

--- src/main.ts
+++ src/main.ts
@@ -3,12 +3,14 @@ import { provideRouter } from '@angular/router';
 import routeConfig from './app/routes';
 import { AppComponent } from './app/app.component';
 import { bootstrapApplication, provideProtractorTestingSupport } from '@angular/platform-browser';
+import { provideHttpClient } from '@angular/common/http';
 
 bootstrapApplication(AppComponent,
   {
     providers: [
       provideProtractorTestingSupport(),
       provideRouter(routeConfig),
+      provideHttpClient()
     ]
   }
 ).catch(err => console.error(err));
