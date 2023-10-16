336e4726503bbead5ae848eb28c3b7bfc7abcfb0
Brenda Lee <brenda.matozo@gmail.com>
2023-10-16 15:00:41 -0300

    feat: creat routes list component

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,10 +1,12 @@
 import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core'; 
+
+import { RoutesListComponent } from './routes-list/routes-list.component';
 import { RouteFormComponent } from './route-form/route-form.component';
 
 @Component({
   standalone: true,
-  imports: [CommonModule, RouteFormComponent],
+  imports: [CommonModule, RoutesListComponent, RouteFormComponent],
   selector: 'app-home',
   templateUrl: './home.component.html',
   styleUrls: ['./home.component.scss'],
---
+++ src/app/home/routes-list/routes-list.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  standalone: true,
+  selector: 'app-routes-list',
+  templateUrl: './routes-list.component.html',
+  styleUrls: ['./routes-list.component.scss'],
+})
+
+export class RoutesListComponent {
+
+}
